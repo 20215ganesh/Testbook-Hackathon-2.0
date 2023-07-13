2e1b15d7cd86c44ddd57434307edf7b67349cc03
20215ganesh <115522440+20215ganesh@users.noreply.github.com>
2023-07-13 17:09:11 +0530

    Add files via upload

--- Drum_kit/index.js
+++ Drum_kit/index.js
@@ -1,66 +1,26 @@
 const kits=['tom1','tom2','tom3','tom4','snare','crash','kick'];
 const text=['w','a','s','d','j','k','l'];
-const containerE=document.querySelector('.container');
-kits.forEach((kit)=>{
-    const btnE=document.createElement('button');
-    containerE.appendChild(btnE);
-    btnE.style.backgroundImage=`url(images/${kit}.png)`;
-    btnE.classList.add('btn');
-    btnE.innerText=text[kits.indexOf(kit)];
-    btnE.addEventListener('click',()=>{
-        if(kit=="tom1"){
-            var sound1 = new Audio("audioSound/tom1.mp3");
-            sound1.play();
-        }else if(kit=="tom2"){
-            var sound1 = new Audio("audioSound/tom2.mp3");
-            sound1.play();
-        }else if(kit=="tom3"){
-            var sound1 = new Audio("audioSound/tom3.mp3");
-            sound1.play();
-        }else if(kit=="tom4"){
-            var sound1 = new Audio("audioSound/tom4.mp3");
-            sound1.play();
-        }else if(kit=="snare"){
-            var sound1 = new Audio("audioSound/snare.mp3");
-            sound1.play();
-        }else if(kit=="crash"){
-            var sound1 = new Audio("audioSound/crash.mp3");
-            sound1.play();
-        }else if(kit=="kick"){
-            var sound1 = new Audio("audioSound/kick.mp3");
-            sound1.play();
-        }
-
-    })
-
-    window.addEventListener('keydown',(event)=>{
-        if(event.key==text[kits.indexOf(kit)]){
-            if(kit=="tom1"){
-                var sound1 = new Audio("audioSound/tom1.mp3");
-                sound1.play();
-            }else if(kit=="tom2"){
-                var sound1 = new Audio("audioSound/tom2.mp3");
-                sound1.play();
-            }else if(kit=="tom3"){
-                var sound1 = new Audio("audioSound/tom3.mp3");
-                sound1.play();
-            }else if(kit=="tom4"){
-                var sound1 = new Audio("audioSound/tom4.mp3");
-                sound1.play();
-            }else if(kit=="snare"){
-                var sound1 = new Audio("audioSound/snare.mp3");
-                sound1.play();
-            }else if(kit=="crash"){
-                var sound1 = new Audio("audioSound/crash.mp3");
-                sound1.play();
-            }else if(kit=="kick"){
-                var sound1 = new Audio("audioSound/kick.mp3");
-                sound1.play();
-            }
-            btnE.style.transform="scale(.9)";
-            setTimeout(()=>{btnE.style.transform="scale(1)";},200)
-            
-
-        }
-    })
-    })
+const containerEl=document.querySelector('.container');
+    kits.forEach((kit) => {
+        const btnEl = document.createElement("button");
+        btnEl.classList.add("btn");
+        btnEl.innerText = kit;
+        btnEl.style.backgroundImage = "url(" + kit + ".png)";
+        containerEl.appendChild(btnEl);
+        btnEl.innerText=text[kits.indexOf(kit)];
+        const audioEl = document.createElement("audio");
+        audioEl.src = "" + kit + ".mp3";
+        containerEl.appendChild(audioEl);
+        btnEl.addEventListener("click", () => {
+          audioEl.play();
+        });
+        window.addEventListener("keydown", (event) => {
+          if (event.key === text[kits.indexOf(kit)]) {
+            audioEl.play();
+            btnEl.style.transform = "scale(.9)";
+            setTimeout(() => {
+              btnEl.style.transform = "scale(1)";
+            }, 100);
+          }
+        });
+      });
